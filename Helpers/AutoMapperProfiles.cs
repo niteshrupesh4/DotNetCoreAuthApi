5d8b230e9571355749aea26689d7c3829cbf4779
Nitesh Kumar <niteshtest500@gmail.com>
2019-02-08 20:22:15 +0530

    Update user

--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -31,6 +31,7 @@ namespace DatingApp.API.Helpers
                      opt.MapFrom(d => d.DateOfBirth.CalculateAge());
                  });
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, ExaltUser>();
         }
     }
 }
